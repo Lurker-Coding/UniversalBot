9dd1203fd78f96fa34fb301c8c251595c62d436b
InternalCosmos <internalcosmos3616@gmail.com>
2019-02-04 07:32:36 -0600

    uwu so neat

--- modules/utils/hastebin.py
+++ modules/utils/hastebin.py
@@ -1,8 +1,16 @@
 import aiohttp
+from json import dumps
 
 
 async def post(content):
     async with aiohttp.ClientSession() as session:
         async with session.post("http://server.cwelch.me/documents", data=str(content).encode('utf-8')) as response:
+            res = await response.json()
+            return f"http://server.cwelch.me/{res['key']}"
+
+
+async def formatted_post(content):
+    async with aiohttp.ClientSession() as session:
+        async with session.post("http://server.cwelch.me/documents", data=str(dumps(content, indent=4)).encode('utf-8')) as response:
             res = await response.json()
             return f"http://server.cwelch.me/{res['key']}"
